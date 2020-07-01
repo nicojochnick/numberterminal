8db51f3531d8bc6c788bf5c0a463817c8e37ce75
Nico Jochnick <nico.jochnick@berkeley.edu>
2020-07-01 12:42:42 -0400

    higher order component stateful

--- src/views/Checkout/Checkout.js
+++ src/views/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -29,7 +29,7 @@ function Copyright() {
     );
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = theme => ({
     appBar: {
         position: 'relative',
     },
@@ -68,7 +68,7 @@ const useStyles = makeStyles((theme) => ({
         marginTop: theme.spacing(3),
         marginLeft: theme.spacing(1),
     },
-}));
+});
 
 const steps = ['Choose a Plan', 'Account Information', 'Porting Information', 'Place Order'];
 
@@ -89,74 +89,102 @@ function getStepContent(step) {
     }
 }
 
-export default function Checkout() {
-    const classes = useStyles();
-    const [activeStep, setActiveStep] = React.useState(0);
 
-    const handleNext = () => {
-        setActiveStep(activeStep + 1);
+
+class Checkout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeStep: 0,
+        };
+        this.handleNext = this.handleNext.bind(this);
+        this.handleBack = this.handleBack.bind(this);
+
+
+    }
+
+    handleNext(){
+        this.setState({
+           activeStep: this.state.activeStep + 1,
+        });
+
     };
 
-    const handleBack = () => {
-        setActiveStep(activeStep - 1);
+    handleBack(){
+        this.setState({
+            activeStep: this.state.activeStep - 1,
+        });
+
+
     };
 
-    return (
-        <React.Fragment style = {{background: 'white'}}>
-            <CssBaseline />
-            <AppBar position="absolute" color="white" className={classes.appBar}>
-                <Toolbar>
-                    <Typography style = {{fontFamily: 'Rubik', fontWeight: 500}} variant="h6" color="inherit" noWrap>
-                        Community Phone
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <main className={classes.layout}>
-                <Paper style={{boxShadow: "0px 0px 0px #919191", }} className={classes.paper}>
-                    <Typography style = {{fontFamily: 'Rubik', fontWeight: 500}} component="h1" variant="h4" align="center">
-                        Self-Transfer
-                    </Typography>
-                    <Stepper activeStep={activeStep} className={classes.stepper}>
-                        {steps.map((label) => (
-                            <Step key={label}>
-                                <StepLabel style = {{fontFamily: 'Rubik', fontWeight: 400}}> {label}</StepLabel>
-                            </Step>))}
-                    </Stepper>
-                    <React.Fragment>
-                        {activeStep === steps.length ? (
-                            <React.Fragment>
-                                <Typography variant="h5" gutterBottom>
-                                    Thank you for your order.
-                                </Typography>
-                                <Typography variant="subtitle1">
-                                    Your order number is #2001539. We have emailed your order confirmation, and will
-                                    send you an update when your order has shipped.
-                                </Typography>
-                            </React.Fragment>
-                        ) : (
-                            <React.Fragment>
-                                {getStepContent(activeStep)}
-                                <div className={classes.buttons}>
-                                    {activeStep !== 0 && (
-                                        <Button onClick={handleBack} className={classes.button}>
-                                            Back
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <React.Fragment style={{background: 'white'}}>
+                <CssBaseline/>
+                <AppBar position="absolute" color="white" className={classes.appBar}>
+                    <Toolbar>
+                        <Typography style={{fontFamily: 'Rubik', fontWeight: 500}} variant="h6" color="inherit" noWrap>
+                            Community Phone
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <main className={classes.layout}>
+                    <Paper style={{boxShadow: "0px 0px 0px #919191",}} className={classes.paper}>
+                        <Typography style={{fontFamily: 'Rubik', fontWeight: 500}} component="h1" variant="h4"
+                                    align="center">
+                            Self-Transfer
+                        </Typography>
+                        <Stepper activeStep={this.state.activeStep} className={classes.stepper}>
+                            {steps.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel style={{fontFamily: 'Rubik', fontWeight: 400}}> {label}</StepLabel>
+                                </Step>))}
+                        </Stepper>
+                        <React.Fragment>
+                            {this.state.activeStep === steps.length ? (
+                                <React.Fragment>
+                                    <Typography variant="h5" gutterBottom>
+                                        Thank you for your order.
+                                    </Typography>
+                                    <Typography variant="subtitle1">
+                                        Your order number is #2001539. We have emailed your order confirmation, and will
+                                        send you an update when your order has shipped.
+                                    </Typography>
+                                </React.Fragment>
+                            ) : (
+                                <React.Fragment>
+                                    {getStepContent(this.state.activeStep)}
+                                    <div className={classes.buttons}>
+                                        {this.state.activeStep !== 0 && (
+                                            <Button onClick={this.handleBack} className={classes.button}>
+                                                Back
+                                            </Button>
+                                        )}
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            onClick={this.handleNext}
+                                            className={classes.button}
+                                        >
+                                            {this.state.activeStep === steps.length - 1 ? 'Place order' : 'Next'}
                                         </Button>
-                                    )}
-                                    <Button
-                                        variant="contained"
-                                        color="primary"
-                                        onClick={handleNext}
-                                        className={classes.button}
-                                    >
-                                        {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                                    </Button>
-                                </div>
-                            </React.Fragment>
-                        )}
-                    </React.Fragment>
-                </Paper>
-                <Copyright />
-            </main>
-        </React.Fragment>
-    );
+                                    </div>
+                                </React.Fragment>
+                            )}
+                        </React.Fragment>
+                    </Paper>
+                    <Copyright/>
+                </main>
+            </React.Fragment>
+        );
+    }
 }
+
+export default withStyles(useStyles)(Checkout);
+
+
+
